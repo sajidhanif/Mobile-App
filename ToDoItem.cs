fcb086c0cc5d350b521ccf88444d5c00e9c1c5bb
sajidhanif <sajidhanif@users.noreply.github.com>
2023-08-05 16:54:21 +0100

    Add files via upload

---
+++ ToDoItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace TodoListApp
+{
+
+	public class TodoItem
+	{
+
+        public string TodoText{ get; set; } 
+		public bool Complete { get; set; }
+
+
+        public TodoItem(string TodoText, bool Complete)
+		{
+			this.TodoText = TodoText;
+			this.Complete = Complete;
+		}
+	}
+}
+
---
+++ TodoListViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TodoListApp
+{
+	public class TodoListViewModel
+	{
+		public ObservableCollection<TodoItem> TodoItems { get; set; }
+
+		public TodoListViewModel()
+		{
+			TodoItems = new ObservableCollection<TodoItem>();
+
+			TodoItems.Add(new TodoItem("Test Completed Item", true));
+			
+		}
+
+		public ICommand AddTodoCommand => new Command(AddTodoItem);
+		public string NewTodoInputValue { get; set; }
+		void AddTodoItem()
+		{
+			TodoItems.Add(new TodoItem(NewTodoInputValue, false));
+		}
+
+		public ICommand RemoveTodoCommand => new Command(RemoveTodoItem);
+		void RemoveTodoItem(object o)
+		{
+			TodoItem todoItemBeingRemoved = o as TodoItem;
+			TodoItems.Remove(todoItemBeingRemoved);
+		}
+
+	}
+}
